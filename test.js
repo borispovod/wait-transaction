bcba7eecbf2740d897550d957f9e738eea2127d8
Boris <boris@crypti.me>
2017-05-31 15:11:59 +0300

    Updated test

--- test.js
+++ test.js
@@ -1,25 +1,49 @@
 /* global describe, it */
-
 const { assert } = require('chai')
 const Web3 = require('web3')
 const web3 = new Web3()
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'))
 const waitTransaction = require('./src')(web3)
 
-/** Returns true if the difference between the two values is less than the given margin of error */
-const within = (margin, a, b) => Math.abs(a - b) < margin
-
 describe('wait-transaction', () => {
-  it('should send ether from one account to another', () => {
-    const [account1, account2] = web3.eth.accounts
-    const balance1 = +web3.eth.getBalance(account1).valueOf()
-    const balance2 = +web3.eth.getBalance(account2).valueOf()
+  it('should send transaction and wait for confirmation by tx id', done => {
+    return new Promise((resolve, reject) => {
+      const [account1, account2] = web3.eth.accounts
 
-    return waitTransaction({ from: account1, to: account2, value: balance1 / 2 }).then(() => {
-      const newBalance1 = +web3.eth.getBalance(account1).valueOf()
-      const newBalance2 = +web3.eth.getBalance(account2).valueOf()
-      assert(within(balance1 / 1e12, newBalance1, balance1 / 2))
-      assert(within(balance2 / 1e12, newBalance2, balance2 + balance1 / 2))
-    })
+      web3.eth.sendTransaction({
+        from: account1,
+        to: account2,
+        value: 1
+      }, (err, tx) => {
+        if (err) {
+          reject(err)
+          return
+        }
+
+        resolve(tx)
+      })
+    }).then(txId => {
+      return waitTransaction(txId).then(blockHash => {
+        return {
+          blockHash,
+          txId
+        }
+      })
+    }).then(opts => {
+      const { txId, blockHash } = opts
+
+      return new Promise((resolve, reject) => {
+        web3.eth.getTransaction(txId, (err, results) => {
+          if (err) {
+            reject(err)
+            return
+          }
+
+          resolve(results.blockHash)
+        })
+      }).then(bH => {
+        assert.equal(blockHash, bH)
+      })
+    }).then(done).catch(done)
   })
 })
